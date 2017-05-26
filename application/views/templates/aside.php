71684e836218ee93895e8d1d6fa5360ae1210903
宋竹梅 <zhumei.song@monstar-lab-cn.com>
2017-05-26 13:49:51 +0800

    5.26 10:11

--- application/views/templates/aside.php
+++ application/views/templates/aside.php
@@ -10,9 +10,9 @@
             </span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a></li>
-          <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-          <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
+          <li><a href=""><i class="fa fa-circle-o"></i> General Elements</a></li>
+          <li><a href=""><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
+          <li><a href=""><i class="fa fa-circle-o"></i> Editors</a></li>
         </ul>
       </li>
     </ul>
